c67b87ce8584dae19de3dda4689c4272179446ec
Henry Chimborazo <henrychimborazo331@gmail.com>
2025-02-23 19:12:06 -0500

    Cambio módulos productos adaptativo y funcionalidades

--- frontend/src/views/categories/ListProducts.jsx
+++ frontend/src/views/categories/ListProducts.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect, useRef   } from "react";
+import { Link, useNavigate  } from "react-router-dom";
 import "./ListProducts.css";
 
 export const ListProducts = () => {
+    const navigate = useNavigate();
+    const hasRedirected = useRef(false);
     const [products, setProducts] = useState([
         { id: "12345678", name: "Estilo Palermo" },
         { id: "12345679", name: "Estilo Madrid" },
@@ -16,44 +18,67 @@ export const ListProducts = () => {
         setProducts(products.filter(product => product.id !== id));
     };
 
+    useEffect(() => {
+        if (hasRedirected.current) return;
+        // Detecta tipo dispositivo
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+        if (isMobile) {
+          alert("Acceso restringido a dispositivos móviles.");
+          hasRedirected.current = true; 
+          navigate("/"); // Redirige a inicio
+        }
+      }, []);
+
     return (
         <>
-            <div className="space">
-                <h2>Panel de administración</h2>
-                <div>
-                    <Link to="/products">
-                        <button><i className="fas fa-arrow-left"></i> Agregar</button>
-                    </Link>
-                    <Link to="/">
-                        <button><i className="fas fa-arrow-left"></i> Regresar</button>
-                    </Link>
+            <div>
+                <div className="flex justify-between items-center bg-[#3C79CF] flex-col md:flex-row">
+                    <h2 className="ml-4 text-xl font-bold text-white">Panel de administración</h2>
+                    <div className="flex gap-3 mr-2flex flex-col md:flex-row gap-1">
+                        <Link to="/products">
+                            <button className="bg-[#AB0D6A] text-white px-6 py-3 rounded-full cursor-pointer font-bold text-lg transition duration-300 hover:bg-pink-700">
+                                <i className="fas fa-plus"></i> Agregar
+                            </button>
+                        </Link>
+                        <Link to="/">
+                            <button className="bg-[#AB0D6A] text-white px-6 py-3 rounded-full cursor-pointer font-bold text-lg transition duration-300 hover:bg-pink-700">
+                                <i className="fas fa-arrow-left"></i> Regresar
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <div className="div-container">
-                <div className="title">
-                    <h3>Listado productos</h3>
-                </div>
-                <div className="table-controler">
-                    <table border="1" width="100%">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nombre</th>
-                                <th>Acción</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(product => (
-                                <tr key={product.id}>
-                                    <td>{product.id}</td>
-                                    <td>{product.name}</td>
-                                    <td>
-                                        <button onClick={() => eliminarFila(product.id)}>Eliminar</button>
-                                    </td>
+
+                <div className="flex flex-col mt-10 px-4">
+                    <div className="text-2xl font-bold mb-4">Listado de productos</div>
+
+                    <div className="mt-8 mb-8 overflow-x-auto">
+                        <table className="w-full min-w-[600px] border-collapse bg-white shadow-md rounded-lg overflow-hidden">
+                            <thead>
+                                <tr className="bg-[#3C79CF] text-white uppercase">
+                                    <th className="p-3">ID</th>
+                                    <th className="p-3">Nombre</th>
+                                    <th className="p-3">Acción</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {products.map((product) => (
+                                    <tr key={product.id} className="even:bg-gray-200 hover:bg-gray-300 transition duration-300">
+                                        <td className="p-3 text-center">{product.id}</td>
+                                        <td className="p-3">{product.name}</td>
+                                        <td className="p-3 text-center">
+                                            <button
+                                                onClick={() => eliminarFila(product.id)}
+                                                className="bg-[#AB0D6A] text-white border-none px-6 py-1 rounded-full cursor-pointer font-bold text-lg transition duration-300 hover:bg-pink-700"
+                                            >
+                                                Eliminar
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </>
--- frontend/src/views/categories/Products.jsx
+++ frontend/src/views/categories/Products.jsx
@@ -1,74 +1,128 @@
-import "./Products.css";
+
+import { useState } from "react"; import "./Products.css";
 import { Link } from "react-router-dom";
 
 export const Products = () => {
+  const [imagePreviews, setImagePreviews] = useState([]);
+
+  const handleImageChange = (event) => {
+    const files = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      const uniqueId = Date.now() + Math.random();
+
+      reader.onloadend = () => {
+        // Guardar todas las imágenes previas y las nuevas en una sola variable
+        setImagePreviews((prevImagePreviews) => [
+          ...prevImagePreviews,
+          { src: reader.result, id: uniqueId },
+        ]);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageRemove = (id) => {
+    setImagePreviews(imagePreviews.filter((image) => image.id !== id));
+  };
 
   return (
     <>
-      <div className="space">
-        <h2>Agregar producto</h2>
+      <div className="flex bg-[#3C79CF] justify-between items-center mb-4">
+        <h2 className="ml-4 font-bold text-white">Agregar producto</h2>
         <Link to="/list-products">
-            <button><i className="fas fa-arrow-left"></i> Regresar</button>
+          <button className="mr-4 mt-2 mb-2 bg-[#AB0D6A] text-white border-none px-6 py-3 rounded-full cursor-pointer font-bold text-lg transition duration-300 hover:bg-[#8A0B57] whitespace-nowrap">
+            <i className="fas fa-arrow-left"></i> Regresar
+          </button>
         </Link>
       </div>
-      <form action="" method="" className="form-container">
-        <div className="form-section">
-          <h3>Información</h3>
-          <div className="form-group">
-            <label form="title">Nombre:</label>
-            <input type="text" name="title" />
+
+      <form className="grid grid-cols-2 gap-8 mx-auto p-4">
+        <div className="flex flex-col gap-4">
+          <h3 className="text-xl font-bold">Información</h3>
+          <div className="flex flex-col">
+            <label htmlFor="title" className="font-bold mb-1">Nombre:</label>
+            <input type="text" name="title" id="title" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group describeSite">
-            <label form="describe">Descripción:</label>
-            <input type="text" name="describe" />
+          <div className="flex flex-col">
+            <label htmlFor="describe" className="font-bold mb-1">Descripción:</label>
+            <input type="text" name="describe" id="describe" className="w-full px-4 py-2 border border-black rounded-lg text-sm h-20" />
           </div>
-          <div className="form-group">
-            <label form="type">Tipo espacio:</label>
-            <input type="text" name="type" />
+          <div className="flex flex-col">
+            <label htmlFor="type" className="font-bold mb-1">Tipo espacio:</label>
+            <input type="text" name="type" id="type" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group">
-            <label form="capacity">Capacidad:</label>
-            <input type="text" name="capacity" />
+          <div className="flex flex-col">
+            <label htmlFor="capacity" className="font-bold mb-1">Capacidad:</label>
+            <input type="text" name="capacity" id="capacity" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group">
-            <label form="price">Precio hora:</label>
-            <input type="text" name="price" />
+          <div className="flex flex-col">
+            <label htmlFor="price" className="font-bold mb-1">Precio hora:</label>
+            <input type="text" name="price" id="price" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
         </div>
 
-        <div className="form-section">
-          <h3>Localización</h3>
-          <div className="form-group">
-            <label form="country">País:</label>
-            <select name="country">
-            <option value="seleccionar" selected disabled>Seleccionar</option>
+        <div className="flex flex-col gap-4">
+          <h3 className="text-xl font-bold">Localización</h3>
+          <div className="flex flex-col">
+            <label htmlFor="country" className="font-bold mb-1">País:</label>
+            <select name="country" id="country" className="w-full px-4 py-2 border border-black rounded-lg text-sm">
+              <option value="seleccionar" disabled selected>Seleccionar</option>
               <option value="argentina">Argentina</option>
               <option value="colombia">Colombia</option>
               <option value="ecuador">Ecuador</option>
             </select>
           </div>
-          <div className="form-group">
-            <label form="region">Región:</label>
-            <input type="text" name="region" />
+          <div className="flex flex-col">
+            <label htmlFor="region" className="font-bold mb-1">Región:</label>
+            <input type="text" name="region" id="region" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group">
-            <label form="suite">Localidad:</label>
-            <input type="text" name="suite" />
+          <div className="flex flex-col">
+            <label htmlFor="suite" className="font-bold mb-1">Localidad:</label>
+            <input type="text" name="suite" id="suite" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group">
-            <label form="zip">Código postal:</label>
-            <input type="text" name="zip" />
+          <div className="flex flex-col">
+            <label htmlFor="zip" className="font-bold mb-1">Código postal:</label>
+            <input type="text" name="zip" id="zip" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
-          <div className="form-group describeSite">
-            <label form="extra">Indicaciones extras:</label>
-            <input type="text" name="extra" />
+          <div className="flex flex-col">
+            <label htmlFor="extra" className="font-bold mb-1">Indicaciones extras:</label>
+            <input type="text" name="extra" id="extra" className="w-full px-4 py-2 border border-black rounded-lg text-sm" />
           </div>
         </div>
 
-        <div className="add-img">
-          <label form="img">Agrega imágenes: </label>
-          <input type="file" multiple accept="image/*" name="img" />
-          <button type="">Guardar</button>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="img" className="font-bold text-lg mb-4">Agrega imágenes:</label>
+          <div>
+            <input type="file" multiple accept="image/*" onChange={handleImageChange} name="img" id="img" className="w-full mt-4 mb-2" />
+            <div className="image-previews flex flex-wrap mt-4">
+              {imagePreviews.map((preview) => (
+                <div key={preview.id} className="relative mb-4 mr-4">
+                  <img
+                    src={preview.src}
+                    alt={`Preview ${preview.id}`}
+                    style={{ width: 100, height: 100, objectFit: 'cover' }}
+                    className="rounded-md"
+                  />
+                  <button
+                    onClick={() => handleImageRemove(preview.id)} // Eliminar la imagen usando su id único
+                    className="absolute top-0 right-0 bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center"
+                    style={{ fontSize: '12px' }}
+                  >
+                    &times;
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-end justify-start mt-4">
+            <button className="bg-[#AB0D6A] text-white px-6 py-2 rounded-full cursor-pointer font-bold text-lg transition duration-300 hover:bg-[#8A0B57]">
+              Guardar
+            </button>
+          </div>
         </div>
       </form>
 
