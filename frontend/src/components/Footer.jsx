f227cf8079a83d53d2839b450bf1afbf84f66a25
Jean Herrera <jeankevinherrera@gmail.com>
2025-02-28 11:30:34 -0500

    Footer modificado de acuerdo a diseño

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,45 +1,79 @@
 import { Link } from "react-router-dom";
-import { FaFacebookF, FaInstagram, FaYoutube, FaWhatsapp } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export const Footer = () => {
-  return (
-    <footer className='bg-[#1E3F75] h-10 '>
-      <div className='flex flex-col p-2 gap-4 md:flex-row items-center max-w-[1200px] mx-auto justify-between'>
-        <div className='items-center justify-center w-full  text-white text-xs font-bold  md:text-sm md:w-auto'>
-        &copy; 2025 CoWork, Inc.
-        </div>
+    const linksLeft = [
+        { name: "Sobre nosotros", link: "/" },
+        { name: "Espacios", link: "/" },
+        { name: "Blog", link: "/" },
+    ];
+    const linksRight = [
+        { name: "Centro de ayuda", link: "/" },
+        { name: "Contacto", link: "/" },
+        { name: "Términos y condiciones", link: "/" },
+    ];
+    return (
+        <footer className="bg-[#111827] px-16 sm:px-10 md:px-20 xl:px-0">
+            <div className="max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 py-12 border-b border-gray-800">
+                <div className="order-1">
+                    <img src="/src/assets/logoLetras.png" alt="Logo" className="pt-2" />
+                    <p className="text-gray-400 mt-6 leading-4">
+                        Encuentra el espacio perfecto para trabajar y crecer tu negocio.
+                    </p>
+                </div>
 
-        <div className='flex gap-4 pr-4 justify-center text-[20px] text-white'>
-          <Link
-            href='https://www.facebook.com/'
-            target='_blank'
-            className='hover:text-gray-400 duration-500'
-          >
-            <FaFacebookF />
-          </Link>
-          <Link
-            href='https://www.instagram.com/'
-            target='_blank'
-            className='hover:text-gray-400 duration-500'
-          >
-            <FaInstagram />
-          </Link>
-          <Link
-            href='https://www.youtube.com/'
-            target='_blank'
-            className='hover:text-gray-400 duration-500'
-          >
-            <FaYoutube />
-          </Link>
-          <Link
-            href='https://web.whatsapp.com'
-            target='_blank'
-            className='hover:text-gray-400 duration-500'
-          >
-            <FaWhatsapp />
-          </Link>
-        </div>
-      </div>
-    </footer>
-  );
+                <div className="order-4 md:order-2">
+                    <p className="text-white text-xl">Enlaces</p>
+                    <ul className="mt-3 flex flex-col gap-2">
+                        {linksLeft.map((link) => (
+                            <li key={link.name}>
+                                <Link to={link.link} className="text-gray-400 hover:text-gray-300 duration-500 ">
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="order-3">
+                    <p className="text-white text-xl">Soporte</p>
+                    <ul className="mt-3 flex flex-col gap-2">
+                        {linksRight.map((link) => (
+                            <li key={link.name}>
+                                <Link to={link.link} className="text-gray-400 hover:text-gray-300 duration-500">
+                                    {link.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="order-2 md:order-4">
+                    <p className="text-white text-xl">Síguenos</p>
+                    <div className="flex gap-4 mt-3 text-gray-400">
+                        <Link
+                            href="https://www.facebook.com/"
+                            target="_blank"
+                            className="hover:text-gray-300 duration-500"
+                        >
+                            <FaFacebook size={20} />
+                        </Link>
+                        <Link href="https://www.x.com/" target="_blank" className="hover:text-gray-300 duration-500">
+                            <FaTwitter size={20} />
+                        </Link>
+                        <Link
+                            href="https://www.instagram.com/"
+                            target="_blank"
+                            className="hover:text-gray-300 duration-500"
+                        >
+                            <FaInstagram size={20} />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="pb-20 pt-4  ">
+                <p className="text-gray-400 text-center py-4">&copy; 2025 CoWork. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    );
 };
