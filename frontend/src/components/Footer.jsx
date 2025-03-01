cce1cf78bc3189d8410c5872e02394c2c68024a4
Jean Herrera <jeankevinherrera@gmail.com>
2025-02-28 20:01:41 -0500

    Plugin de ordenamiento de clases de tailwind agregado

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,20 +14,20 @@ export const Footer = () => {
     ];
     return (
         <footer className="bg-[#111827] px-16 sm:px-10 md:px-20 xl:px-0">
-            <div className="max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 py-12 border-b border-gray-800">
+            <div className="mx-auto grid max-w-[1200px] grid-cols-1 gap-8 border-b border-gray-800 py-12 sm:grid-cols-2 md:grid-cols-4">
                 <div className="order-1">
                     <img src="/src/assets/logoLetras.png" alt="Logo" className="pt-2" />
-                    <p className="text-gray-400 mt-6 leading-4">
+                    <p className="mt-6 leading-4 text-gray-400">
                         Encuentra el espacio perfecto para trabajar y crecer tu negocio.
                     </p>
                 </div>
 
                 <div className="order-4 md:order-2">
-                    <p className="text-white text-xl">Enlaces</p>
+                    <p className="text-xl text-white">Enlaces</p>
                     <ul className="mt-3 flex flex-col gap-2">
                         {linksLeft.map((link) => (
                             <li key={link.name}>
-                                <Link to={link.link} className="text-gray-400 hover:text-gray-300 duration-500 ">
+                                <Link to={link.link} className="text-gray-400 duration-500 hover:text-gray-300">
                                     {link.name}
                                 </Link>
                             </li>
@@ -36,11 +36,11 @@ export const Footer = () => {
                 </div>
 
                 <div className="order-3">
-                    <p className="text-white text-xl">Soporte</p>
+                    <p className="text-xl text-white">Soporte</p>
                     <ul className="mt-3 flex flex-col gap-2">
                         {linksRight.map((link) => (
                             <li key={link.name}>
-                                <Link to={link.link} className="text-gray-400 hover:text-gray-300 duration-500">
+                                <Link to={link.link} className="text-gray-400 duration-500 hover:text-gray-300">
                                     {link.name}
                                 </Link>
                             </li>
@@ -49,30 +49,30 @@ export const Footer = () => {
                 </div>
 
                 <div className="order-2 md:order-4">
-                    <p className="text-white text-xl">Síguenos</p>
-                    <div className="flex gap-4 mt-3 text-gray-400">
+                    <p className="text-xl text-white">Síguenos</p>
+                    <div className="mt-3 flex gap-4 text-gray-400">
                         <Link
                             href="https://www.facebook.com/"
                             target="_blank"
-                            className="hover:text-gray-300 duration-500"
+                            className="duration-500 hover:text-gray-300"
                         >
                             <FaFacebook size={20} />
                         </Link>
-                        <Link href="https://www.x.com/" target="_blank" className="hover:text-gray-300 duration-500">
+                        <Link href="https://www.x.com/" target="_blank" className="duration-500 hover:text-gray-300">
                             <FaTwitter size={20} />
                         </Link>
                         <Link
                             href="https://www.instagram.com/"
                             target="_blank"
-                            className="hover:text-gray-300 duration-500"
+                            className="duration-500 hover:text-gray-300"
                         >
                             <FaInstagram size={20} />
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className="pb-20 pt-4  ">
-                <p className="text-gray-400 text-center py-4">&copy; 2025 CoWork. Todos los derechos reservados.</p>
+            <div className="pb-20 pt-4">
+                <p className="py-4 text-center text-gray-400">&copy; 2025 CoWork. Todos los derechos reservados.</p>
             </div>
         </footer>
     );
