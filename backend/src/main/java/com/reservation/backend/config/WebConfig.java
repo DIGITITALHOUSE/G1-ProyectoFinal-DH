b734fc16cce03baeeaa3fcf20d649d00973e2a51
Pablo Bellido <bellido.peru26@gmail.com>
2025-03-06 17:22:54 -0500

    fix(cors): add enable web mvc config cors

--- backend/src/main/java/com/reservation/backend/config/WebConfig.java
+++ backend/src/main/java/com/reservation/backend/config/WebConfig.java
@@ -2,14 +2,19 @@ package com.reservation.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    public void addCorsMappings(CorsRegistry registry){
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT","DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
