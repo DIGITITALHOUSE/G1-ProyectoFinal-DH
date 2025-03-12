3a24077f7f5289054720d9814c00113d065d92e1
Ailin <ailindoria80@gmail.com>
2025-03-12 00:40:54 -0500

    integracion de s3 en los controladores image y space

--- backend/src/main/java/com/reservation/backend/controllers/ImageController.java
+++ backend/src/main/java/com/reservation/backend/controllers/ImageController.java
@@ -1,15 +1,19 @@
 package com.reservation.backend.controllers;
 
+import com.reservation.backend.entities.Space;
 import com.reservation.backend.entities.SpaceImage;
 import com.reservation.backend.repositories.ISpaceImageRepository;
+import com.reservation.backend.repositories.ISpaceRepository;
 import com.reservation.backend.services.impl.S3Service;
 import com.reservation.backend.services.impl.SpaceImageService;
+import com.reservation.backend.services.impl.SpaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/imagenes")
@@ -17,18 +21,18 @@ import java.util.List;
 public class ImageController {
 
     private final S3Service s3Service;
-    private final ISpaceImageRepository spaceImageRepository;
 
+    private final ISpaceImageRepository spaceImageRepository;
 
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file, @RequestParam("spaceId") Long spaceId) {
         try {
 
-            String s3Path = s3Service.uploadImage(file);
+            String url = s3Service.uploadImage(file);
 
 
             SpaceImage spaceImage = new SpaceImage();
-            spaceImage.setS3Path(s3Path);
+            spaceImage.setUrl(url);
             spaceImageRepository.save(spaceImage);
 
 
@@ -46,7 +50,7 @@ public class ImageController {
 
 
             var preSignedUrls = images.stream()
-                    .map(image -> s3Service.generatePreSignedURL(image.getS3Path()))
+                    .map(image -> s3Service.generatePreSignedURL(image.getUrl()))
                     .toList();
 
             return ResponseEntity.ok(preSignedUrls);
--- backend/src/main/java/com/reservation/backend/controllers/SpaceController.java
+++ backend/src/main/java/com/reservation/backend/controllers/SpaceController.java
@@ -1,9 +1,13 @@
 package com.reservation.backend.controllers;
 
+import com.reservation.backend.dtos.SpaceImageRequestDto;
 import com.reservation.backend.dtos.SpaceRequestDto;
 import com.reservation.backend.dtos.SpaceRequestToUpdateDto;
 import com.reservation.backend.dtos.SpaceResponseDto;
+import com.reservation.backend.entities.SpaceImage;
+import com.reservation.backend.services.ISpaceImageService;
 import com.reservation.backend.services.ISpaceService;
+import com.reservation.backend.services.impl.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
@@ -11,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 // import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -19,10 +24,15 @@ import java.util.List;
 @RequestMapping("/spaces")
 @CrossOrigin(origins = "*")
 public class SpaceController {
+    private final ISpaceImageService spaceImageService;
     private final ISpaceService spaceService;
+    private final S3Service s3Service;
 
-    public SpaceController(ISpaceService spaceService) {
+    public SpaceController(ISpaceImageService spaceImageService,ISpaceService spaceService,S3Service s3Service) {
+
+        this.spaceImageService = spaceImageService;
         this.spaceService = spaceService;
+        this.s3Service = s3Service;
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -32,6 +42,20 @@ public class SpaceController {
     ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(spaceService.create(requestDto));
     }
+
+    @PostMapping("/{id}/images")
+    public ResponseEntity<List<SpaceImage>>uploadImages(@PathVariable Long spaceId, @RequestParam("images")List<MultipartFile>images){
+        SpaceImageRequestDto requestDto = new SpaceImageRequestDto();
+        requestDto.setSpaceId(spaceId);
+        requestDto.setImages(images);
+
+        List<SpaceImage> spaceImages = spaceImageService.mapToEntity(requestDto, s3Service);
+
+        List<SpaceImage> savedImages = spaceImageService.save(spaceImages);
+
+        return ResponseEntity.ok(savedImages);
+    }
+
 
     @GetMapping
     public ResponseEntity<List<SpaceResponseDto>> findAll() {
@@ -55,4 +79,5 @@ public class SpaceController {
         spaceService.delete(id);
         return ResponseEntity.noContent().build();
     }
+
 }
